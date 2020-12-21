4a292bde79a246760f3fd81ec1d969be0e1e52aa
ndzda <ndzda>
2020-12-21 11:39:36 +0800

    ud

---
+++ src/main.cpp
@@ -0,0 +1,12 @@
+#include "io/io.cpp"
+#include "vm/vm.cpp"
+
+
+
+int main(int argc,char *argv[])
+{
+    if(argc<=1)
+        error("Please pass in the binary file name as the startup parameter.");
+    
+    return 0;
+}
